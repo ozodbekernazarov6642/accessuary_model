cd4bddaf983b931ae4e1ce059016aafae77c3c78
ozodbekernazarov6642 <128281409+ozodbekernazarov6642@users.noreply.github.com>
2025-08-11 18:59:47 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from fastai.vision.all import *
 import streamlit as st
-import plotly.express as px
 import pathlib
+import plotly.express as px
+plt = platform.system()
+if plt == "Linux": pathlib.WindowsPath = pathlib.PosixPath
 
-# WindowsPath ni Linuxda ishlashga moslashtirish
-pathlib.WindowsPath = pathlib.PosixPath
-
-# Klass nomlarini tarjima qilish
 product = {
     "Watch": "Soat",
     "Umbrella": "Soyabon",
@@ -14,42 +12,19 @@ product = {
     "Glasses": "Ko'zoynak"
 }
 
-# Sarlavha va tavsif
-st.title("Inson o'zi uchun qo‘llaydigan aksessuarlarni klassifikatsiya qilish")
-st.markdown(
-    """
-    Klassifikatsiya qilinadigan mahsulotlar:
-    <b><i>Ko'zoynak</i></b>, <b><i>Toj</i></b>, <b><i>Soat</i></b>, <b><i>Soyabon</i></b>
-    """,
-    unsafe_allow_html=True
-)
+st.title('Inson o\'zi uchun qollaydian aksessuarlarni klassifikatsiya qilish')
+st.markdown('Klassificatsiya qilinadiga maxsulotlar: <b><i>Ko\'zoynak</i></b>, <b><i>Toj</i></b>, <b><i>Soat</i></b>, '
+            '<b><i>Soyabon</i></b>', unsafe_allow_html=True)
 
-# Fayl yuklash
-file = st.file_uploader("Rasm yuklang", type=['webp', 'png', 'jpg', 'jpeg'])
-
+file = st.file_uploader("Rasm yuklang", type=['webp', 'png', 'jpg'])
 if file:
-    # Modelni yuklash
     model = load_learner("accessory_model.pkl")
-
-    # Rasmdan PILImage yaratish
     img = PILImage.create(file)
-
-    # Bashorat qilish
     pred, pred_id, probs = model.predict(img)
-
-    # Ehtimollik tekshirish
     if probs[pred_id] * 100 < 90:
-        st.image(img, width=200, caption="Rasmingiz ushbu toifalarga to‘g‘ri kelmaydi")
+        st.image(img, width=200, caption=["Rasmingizni bu toifalarga tog'ri kelmaydi"])
     else:
-        st.image(img, width=200, caption=f"Bashorat: {product[pred]}")
         st.success(f"Bashorat: {product[pred]}")
         st.info(f"Ehtimollik: {probs[pred_id] * 100:.2f}%")
-
-        # Ehtimolliklarni grafikda ko‘rsatish
-        fig = px.bar(
-            x=[product[c] for c in product.keys()],
-            y=probs * 100,
-            labels={'x': "Klasslar", "y": "Ehtimollik (%)"},
-            title="Klassifikatsiya ehtimolliklari"
-        )
+        fig = px.bar(x=['Toj', "Ko'zoynak", "Soyabon", "Soat"], y=probs * 100, labels={'x':"Klasslar", "y":"Ehtimollik"})
         st.plotly_chart(fig)
